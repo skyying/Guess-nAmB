2999f13ae7544a914e07e0856d4a6b92411e9e17
leslie_liu <leslie_liu@trendmicro.com>
2020-08-22 00:45:51 +0800

    Initial Commit

---
+++ Guess_nAmB/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Guess_nAmB
+//
+//  Created by leslie_liu on 2020/8/22.
+//  Copyright © 2020 skyying. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
